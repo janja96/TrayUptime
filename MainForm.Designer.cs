44a8343b87eaebee78c1c91ba1fb28c92c46590d
whit33r <whit33r@gmail.com>
2013-03-28 21:44:45 +0100

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TrayUptime
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.NotifyUp = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsUp = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.autorunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.bwUp = new System.ComponentModel.BackgroundWorker();
+            this.cmsUp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NotifyUp
+            // 
+            this.NotifyUp.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.NotifyUp.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifyUp.Icon")));
+            this.NotifyUp.Visible = true;
+            this.NotifyUp.MouseClick += new System.Windows.Forms.MouseEventHandler(this.nofityUp_MouseClick);
+            // 
+            // cmsUp
+            // 
+            this.cmsUp.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.autorunToolStripMenuItem,
+            this.exitToolStripMenuItem1});
+            this.cmsUp.Name = "contextMenuStrip1";
+            this.cmsUp.Size = new System.Drawing.Size(132, 52);
+            // 
+            // autorunToolStripMenuItem
+            // 
+            this.autorunToolStripMenuItem.Name = "autorunToolStripMenuItem";
+            this.autorunToolStripMenuItem.Size = new System.Drawing.Size(131, 24);
+            this.autorunToolStripMenuItem.Text = "Autorun";
+            this.autorunToolStripMenuItem.Click += new System.EventHandler(this.autorunToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem1
+            // 
+            this.exitToolStripMenuItem1.Image = global::TrayUptime.Properties.Resources.Windows_Close_Program_icon;
+            this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(131, 24);
+            this.exitToolStripMenuItem1.Text = "Exit";
+            this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // bwUp
+            // 
+            this.bwUp.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bwUp_DoWork);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(140, 0);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.cmsUp.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon NotifyUp;
+        private System.Windows.Forms.ContextMenuStrip cmsUp;
+        private System.ComponentModel.BackgroundWorker bwUp;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem autorunToolStripMenuItem;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace TrayUptime
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            NotifyUp.ShowBalloonTip(1000, "System Uptime", "Run", ToolTipIcon.Info);
+
+            bwUp.RunWorkerAsync();
+
+            if (Startup())
+            {
+                autorunToolStripMenuItem.Checked = true;
+            }
+            else
+            {
+                autorunToolStripMenuItem.Checked = false;
+            }
+        }
+
+        private void nofityUp_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right || e.Button == MouseButtons.Left)
+            {
+                NotifyUp.ContextMenuStrip = cmsUp;
+                MethodInfo mi = typeof (NotifyIcon).GetMethod("ShowContextMenu",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+                mi.Invoke(NotifyUp, null);
+                NotifyUp.ContextMenuStrip = cmsUp;
+            }
+        }
+
+        private void AddAutoRun(bool del)
+        {
+            RegistryKey App = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (!Startup())
+                App.SetValue("Uptime", Application.ExecutablePath);
+            if (del)
+            {
+                App.DeleteValue("Uptime", false);
+            }
+        }
+
+        private static bool Startup()
+        {
+            RegistryKey App = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (App.GetValue("Uptime") == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void bwUp_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (;;)
+            {
+                Thread.Sleep(1000);
+                string uptime = "";
+                uptime += "Uptime: " + (Environment.TickCount/86400000) + " days, ";
+                uptime += (Environment.TickCount/3600000%24) + " hours, ";
+                uptime += (Environment.TickCount/120000%60) + " minutes, ";
+                uptime += (Environment.TickCount/1000%60) + " seconds.";
+                NotifyUp.Text = uptime;
+            }
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void autorunToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (autorunToolStripMenuItem.Checked)
+            {
+                autorunToolStripMenuItem.Checked = false;
+                AddAutoRun(true);
+            }
+            else
+            {
+                autorunToolStripMenuItem.Checked = true;
+                AddAutoRun(false);
+            }
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            Hide();
+        }
+    }
+}
